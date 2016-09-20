b85e93850aa2cc3c88145fa67bbae64a9e754e60
larsmeulenbroek <larsmeulenbroek1997@hotmail.com>
2016-09-20 17:40:28 +0200

    update on bestemming

---
+++ script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Lars Meulenbroek on 9/20/2016.
+ */
+
+// window.sample = function () {
+//     alert("succes");
+//     document.getElementById("test1").style.width = "75%";
+//     document.getElementById("test2").style.display = "block";
+//     document.getElementById("test2").style.float = "right";
+//
+// };
+
