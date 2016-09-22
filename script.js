4fa90912f9a7d1814d3c3eea9d914f7819309801
larsmeulenbroek <larsmeulenbroek1997@hotmail.com>
2016-09-22 15:30:39 +0200

    index fix and bestemming responsive

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function init() {
+    showSlides();
+
+}
+
+var myIndex = 0;
+
+function showSlides() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
